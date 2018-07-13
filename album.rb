8861a867418c261ce4fd8b259da7b7365a31433c
tammy0508 <tamara.dandreamatteo@gmail.com>
2018-07-13 17:36:50 +0000

    automatically backed up by learn

--- album.rb
+++ album.rb
@@ -2,7 +2,15 @@
 
 class Album
 
-  @@album_count = 0 
+  @@album_count = 0
+
+  def initialize
+    @@album_count += 1
+  end 
+
+  def self.count
+    @@album_count
+  end  
 
   def release_date=(date)
     @release_date = date
